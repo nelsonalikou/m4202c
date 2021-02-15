e0fe95a226a6a0d2a8ec4da90008161bbc25b27f
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-02-15 18:34:00 +0100

    fin Exercice 4, début exercice 5, arret petit b

--- TP03.sql
+++ TP03.sql
@@ -62,3 +62,76 @@ SELECT NVL(COUNT(c.CHB_ID),0) as "nb chambres"
     FROM CHAMBRE c, AGENT_ENTRETIEN a  
     WHERE c.AGT_ID(+) = a.AGT_ID 
     AND UPPER(a.AGT_NOM) = UPPER('DUSSE');  --FECHOL DUSSE  BEVIERE
+    
+    
+--Exercice 4 Variables %TYPE – Variables de substitution – Exception Oracle prédéfinie 
+
+-- bloc PL/SQL qui, étant donné le nom d’un client saisi au clavier (ACCEPT – PROMPT), affiche son code, son identité (nom, prénom) et sa ville.
+
+accept cli_name prompt 'Please enter your name: ';
+
+--Amélioration lisibilité
+SET VERIFY OFF;
+
+declare
+   id CLIENT.CLI_ID%TYPE;
+   nom CLIENT.CLI_NOM%TYPE;
+   prenom CLIENT.CLI_PRENOM%TYPE;
+   ville ADRESSE.ADR_VILLE%TYPE;
+   name CLIENT.CLI_NOM%TYPE;
+   nb INTEGER;
+begin
+    name := '&cli_name';
+    SELECT c.CLI_ID, UPPER(c.CLI_NOM), c.CLI_PRENOM,a.ADR_VILLE, COUNT(c.CLI_ID)
+    INTO id, nom, prenom, ville, nb   
+    FROM CLIENT c, ADRESSE a
+    WHERE c.CLI_ID = a.CLI_ID
+    AND UPPER(CLI_NOM) = UPPER('&cli_name') 
+    group by c.CLI_ID, UPPER(c.CLI_NOM), c.CLI_PRENOM, a.ADR_VILLE;
+    IF(SQL%ROWCOUNT > 1)
+    THEN dbms_output.put_line ( 'Attention ! Plusieurs clients nommés ' || name);
+    END IF;
+    dbms_output.put_line('résultats pour '|| nom);
+    
+    dbms_output.put_line('le client '|| id || ' ' || prenom || REPLACE(nom,' ','') || ' habite à ' || ville);
+EXCEPTION
+    WHEN NO_DATA_FOUND
+    THEN dbms_output.put_line ( 'Désolé, pas de client nommé ' || name);
+end;
+/
+
+-- Exercice 5 Gestion des EXCEPTIONS, erreurs prédéfinies et non prédéfinies ORACLE
+
+-- a)
+
+accept agt_code prompt 'Please enter the agt code ';
+
+accept date_depart prompt 'Please enter the new departure date: ';
+
+DECLARE
+    agt_cod AGENT_ENTRETIEN.AGT_ID%TYPE;
+    date_dpt AGENT_ENTRETIEN.AGT_DPT%TYPE;
+  
+Begin
+    
+    agt_cod := '&agt_code';
+    date_dpt := TO_DATE('&date_depart', 'DD/MM/YYYY');
+    
+    UPDATE AGENT_ENTRETIEN
+    SET AGT_DPT = date_dpt
+    WHERE AGT_ID = agt_cod;
+    dbms_output.put_line('L''agent a été modifié');
+EXCEPTION
+    WHEN NO_DATA_FOUND
+    THEN dbms_output.put_line ( 'L''agent ' || agt_cod || ' n''existe pas' );
+    WHEN OTHERS 
+    THEN raise_application_error(-20020,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+end;
+/
+
+
+-- Test
+--AGT_ID = A08 : error agent inexistant
+--AGT_ID = A05 et AGT_DPT = 01/15/2021 : error ce n'est pas un mois valide 
+-- puis avec AGT_DPT 01/06/2021 - Success
+
